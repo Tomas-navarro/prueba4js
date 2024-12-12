9639b1963e33a822a701a1283146a699f701439d
Tomas Navarro <tomas.navarro.m@outlook.com>
2024-12-11 23:40:54 -0300

    add js al index con sus funcionalidades

---
+++ assets/js/data.js
@@ -0,0 +1,92 @@
+export const propiedades_alquiler = [
+    {
+        nombre: 'Casa de Campo',
+        src: 'https://plus.unsplash.com/premium_photo-1666241368300-a1115df75b7c?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Una hermosa casa de campo rodeada de naturaleza.',
+        ubicacion: 'Valle de las Flores',
+        habitaciones: 3,
+        costo: 1200,
+        smoke: false,
+        pets: true,
+        baños:3
+    },
+    {
+        nombre: 'Apartamento Urbano',
+        src: 'https://images.unsplash.com/photo-1518729208902-794dc50abea0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Moderno apartamento en el corazón de la ciudad.',
+        ubicacion: 'Centro de la Ciudad',
+        habitaciones: 2,
+        costo: 1500,
+        smoke: true,
+        pets: false,
+        baños:4
+    },
+    {
+        nombre: 'Cabaña en la Montaña',
+        src: 'https://images.unsplash.com/photo-1520637645622-6740bb3491c3?q=80&w=1916&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Cabaña acogedora con vistas panorámicas a la montaña.',
+        ubicacion: 'Sierra Nevada',
+        habitaciones: 4,
+        costo: 1800,
+        smoke: false,
+        pets: true,
+        baños:2
+    },
+    {
+        nombre: 'Cabaña de Playa',
+        src: 'https://images.unsplash.com/photo-1518729208902-794dc50abea0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Cabaña humilde a pocos pasos de la playa.',
+        ubicacion: 'Centro de la Ciudad',
+        habitaciones: 2,
+        costo: 1500,
+        smoke: true,
+        pets: false,
+        baños:1
+    }
+];
+export const propiedades_ventas = [
+    {
+        nombre: 'Villa en la Playa',
+        src: 'https://plus.unsplash.com/premium_photo-1661905792999-036ddbe402a5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Espectacular villa frente al mar con piscina privada.',
+        ubicacion: 'Costa del Sol',
+        habitaciones: 5,
+        costo: 850000,
+        smoke: true,
+        pets: true,
+        baños:2
+    },
+    {
+        nombre: 'Piso Moderno',
+        src: 'https://plus.unsplash.com/premium_photo-1674676471417-07f613528a94?q=80&w=2045&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Amplio piso con diseño contemporáneo y acabados de lujo.',
+        ubicacion: 'Zona Financiera',
+        habitaciones: 3,
+        costo: 420000,
+        smoke: false,
+        pets: false,
+        baños:3
+    },
+    {
+        nombre: 'Casa Rural',
+        src: 'https://images.unsplash.com/photo-1622268886137-9be86b6e8e4e?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Encantadora casa rural rodeada de viñedos.',
+        ubicacion: 'La Rioja',
+        habitaciones: 4,
+        costo: 320000,
+        smoke: false,
+        pets: true,
+        baños:2
+    },
+    {
+        nombre: 'Cabaña de Playa',
+        src: 'https://images.unsplash.com/photo-1518729208902-794dc50abea0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Cabaña humilde a pocos pasos de la playa.',
+        ubicacion: 'Centro de la Ciudad',
+        habitaciones: 2,
+        costo: 1500,
+        smoke: true,
+        pets: false,
+        baños:1
+    }
+];
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+import { propiedades_alquiler, propiedades_ventas } from "./data.js";
+
+function renderizarPropiedades (propiedades, contenedorId, limite){
+
+    const contenedor = document.getElementById(contenedorId)
+
+    let contenidoHTML = "";
+
+    for (let index = 0; index < propiedades.length && index < limite; index++) {
+        const propiedad = propiedades[index];
+        
+        console.log ("entre")
+        let propiedadHTML= `
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img
+                src="${propiedad.src}"
+                class="card-img-top"
+                alt="Imagen del departamento"
+                />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${propiedad.nombre}
+                </h5>
+                <p class="card-text">
+                  ${propiedad.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i>${propiedad.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${propiedad.habitaciones} |
+                  <i class="fas fa-bath"></i> ${propiedad.baños}
+                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
+                ${propiedad.smoke === true ? '<p class="text-success"><i class="fas fa-smoking"></i> Permitido fumar </p>' : '<p class="text-danger"> <i class="fas fa-smoking-ban"></i> No se permite fumar </p>'}
+                ${propiedad.pets === true ? '<p class="text-success"> <i class="fas fa-paw"></i> Mascotas permitidas </p>' : '<p class="text-danger"> <i class="fa-solid fa-ban"></i> No se permiten mascotas </p>'}
+              </div>
+            </div>
+          </div>
+        `;
+        contenidoHTML += propiedadHTML;
+    }
+    contenedor.innerHTML = contenidoHTML
+}
+window.onload = () =>{
+    renderizarPropiedades(propiedades_ventas,"propiedades-ventas-container", 3)
+    renderizarPropiedades(propiedades_alquiler,"propiedades-alquiler-container", 3)
+}
